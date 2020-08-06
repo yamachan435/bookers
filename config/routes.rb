28f62cc431b9849b5dce3c785a4371c5a8c6f96e
vagrant <vagrant@localhost.localdomain>
2020-08-06 16:53:19 +0000

    トップとabout作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#top'
   devise_for :users
   resources :books, except: [:new]
   resources :users, only: [:index, :show, :edit, :update]
+  get '/home/about', to: 'home#about'
 end
