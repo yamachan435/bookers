8366e2aebf963bca07e2e1d5876be7131d527d25
vagrant <vagrant@localhost.localdomain>
2020-08-06 15:23:50 +0000

    ユーザ操作機能の追加。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+    render layout: 'mypage'
+  end
+
+  def show
+    render layout: 'user_page'
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :introduction)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books, except: [:new]
+  resources :users, only: [:index, :show, :edit, :update]
 end
