59bf6082b7d8dbc8f07247bab6c273042f5fef53
vagrant <vagrant@localhost.localdomain>
2020-09-05 18:08:02 +0000

    [add] Routes about books/favorite.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
-  resources :books, except: [:new]
+  resources :books, except: [:new] do
+    scope module: :books do
+      resources :favorites, only: [:index] do
+        get 'pickup', on: :collection
+      end
+      resource :favorite, only: [:create, :destroy]
+    end
+  end
   resources :users, only: [:index, :show, :edit, :update]
   get '/home/about', to: 'home#about'
 end
