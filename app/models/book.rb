143b913ad11a87dafc8bca9aacf9c8b131303fa9
vagrant <vagrant@localhost.localdomain>
2020-09-05 18:05:25 +0000

    [add] Method about favorite on Book.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
   validates :body, length: { maximum: 200 }
 
   belongs_to :user
+  has_many :favorites
+  has_many :favoriting_users, through: :favorites, source: :user
+
+  def favorited_by?(user)
+    favoriting_users.include?(user)
+  end
 end
